658a683e31bad7277648c604a1b8ac35d733a8c2
Jeremy Desktop <jeremycrisostomo@gmail.com>
2025-05-16 09:12:36 -0700

    updated script for long option plays, exports to CSV in 'export/', included fees

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
+import csv
+
 """
 Main Program
 """
 
 """
-This function will iterate through the list and print out starting capital, utilization, and expected perfect take profit, and ending balance.
+This function will iterate through the list and print out starting
+capital, utilization, and expected perfect take profit, and ending
+balance.
 
-Assume first trade maxes out utilization percent. Each iteration will run through a spread trade until the (sum of an additional spread trade) is less than the capital utilization max.
+Assume first trade maxes out utilization percent. Each iteration will
+run through a trade trade until the (sum of an additional trade trade)
+is less than the capital utilization max.
 """
-def main(threshold, cap_start, util_pct_max, adj_gain, spread_cost):
+def main(threshold, cap_start, util_pct_max, adj_gain, contract_cost, platform_fee):
+
+    file_path = 'export/export_scratch.csv'
+
     meta_list = []
     # init portfolio and trade costs
     portfolio = cap_start
-    trade_cost = spread_cost
+    contract_amt = 1
     # define iteration end
     while portfolio < threshold:
         # track amount of contracts being traded per spread
-        contract_amt = 1
         # add an additional contract if portfolio utilization allows
-        if (trade_cost + spread_cost) <= (portfolio * util_pct_max):
-            trade_cost += spread_cost
-            contract_amt += 1
+        level = contract_cost * (contract_amt + 1)
+        capacity = portfolio * util_pct_max
+        if level <= capacity:
+            while level <= capacity:
+                level += contract_cost
+                contract_amt += 1
 
-        def mathing(portfolio, trade_cost, contract_amt):
+        def mathing(portfolio, contract_amt):
             # define actual_utilization
-            actual_util = trade_cost / portfolio
+            actual_cost = contract_cost * contract_amt
+            actual_util = actual_cost / portfolio
             # define profits
-            profit = trade_cost * adj_gain
+            profit = actual_cost * adj_gain
             # define ending portfolio
-            end_port = portfolio + profit
-            transaction = [portfolio, contract_amt, trade_cost, actual_util, profit, end_port]
+            fees = contract_amt * 2 * platform_fee
+            end_port = portfolio + profit - fees
+            transaction = [portfolio, contract_amt, actual_cost,
+                           actual_util, profit, fees, end_port]
             meta_list.append(transaction)
             return end_port
 
-        portfolio = mathing(portfolio, trade_cost, contract_amt)
-    for index, item in enumerate(meta_list):
-        print(index, item)
+        portfolio = mathing(portfolio, contract_amt)
+    transaction_header = [["Portfolio", "Contract Amount", "Trade Cost",
+                          "Portfolio Utilization", "Profit", "Fees",
+                          "Ending Balance"]]
+    data = transaction_header + meta_list
+    
+    with open(file_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
 
 if __name__ == "__main__":
-    threshold = 25000 # monetary goal
-    cap_start = 1345 # starting portfolio
+    threshold = 1000000 # monetary goal
+    cap_start = 300 # starting portfolio
     util_pct_max = 0.55 # maximum portfolio utilization percent
-    adj_gain = 0.28 #
-    spread_cost = 740 # typical spread max risk
+    adj_gain = 0.1 # profit percentage in decimal
+    contract_cost = 150 # typical trade max risk
+    platform_fee = 0.65# variable for platform fee (RH = .08, CS = 0.65)
     print("running program")
-    main(threshold, cap_start, util_pct_max, adj_gain, spread_cost)
+    main(threshold, cap_start, util_pct_max, adj_gain, contract_cost, platform_fee)
