f4160c519c148783b2d0e62ad7989bc98b41de13
Jeremy Desktop <jeremycrisostomo@gmail.com>
2025-10-26 04:44:19 -0700

    added compound_return_pct into prompt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import platform
 import fixed_return_pct
+import compound_return_pct
 
 
 def main():
@@ -9,12 +10,15 @@ def main():
 
     while True:
         user_choice = input("""Select a program:
-        1) Fixed Return
+        1) Fixed Returns
+        2) Compound Returns
         Q) Exit
         
         : """)
         if user_choice == "1":
             fixed_return_pct.main(os_type)
+        elif user_choice == "2":
+            compound_return_pct.main(os_type)
         elif user_choice.upper() == "Q":
             return False
         else:
